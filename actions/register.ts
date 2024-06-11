bb9f4c9b75b3dced534fb01c21d731aa666e0852
Woody97442 <woody97442@hotmail.fr>
2024-06-12 00:50:47 +0400

    Ajoute de prisma model avec shema user et account plus la migration et initialisation de la bdd en mysql

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 "use server";
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+import { prisma } from "@/prisma/prismaClient";
 
 export const register = async (value: z.infer<typeof RegisterSchema>) => {
     const validatedField = RegisterSchema.safeParse(value);
@@ -9,5 +11,28 @@ export const register = async (value: z.infer<typeof RegisterSchema>) => {
         return { error: "Invalid fields !" };
     }
 
-    return { success: "Email sent !" };
+    const { email, username, password } = validatedField.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if (existingUser) {
+        return { error: "Email already exists !" };
+    }
+
+    await prisma.user.create({
+        data: {
+            email,
+            username,
+            passwordHash: hashedPassword,
+        },
+    });
+
+    // Send verification token email
+
+    return { success: "User created !" };
 }
