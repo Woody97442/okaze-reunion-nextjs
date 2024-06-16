3f536bf393fea4570e50a49a094aed7ff4f2872c
Woody97442 <woody97442@hotmail.fr>
2024-06-16 23:24:31 +0400

    Ajoute de la fonctionnalité de mots de passe oublié terminer
    Ajoute du model prisma PasswordResetToken
    
    Ajoute des shemas zod NewPassword et ResetSchema
    
    Ajouter des functions
    getPasswordResetTokenByEmail
    sendPasswordResetEmail
    
    Ajoute de la page reset form et new password
    
    Ajoute du link mots de passe oublié sur le form de login
    
    Ajoute des action reset et new-password
    
    Ajoute des route
    "/auth/reset",
    "/auth/new-password"
    sur authRoutes

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -9,12 +9,11 @@ import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-success";
 
 const NewVerificationForm = () => {
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
-
-  const searchParams = useSearchParams();
-
-  const token = searchParams.get("token");
 
   const onSubmit = useCallback(() => {
     if (success || error) return;
