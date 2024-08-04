39376133c88a1ad7f10cffd3ad6b74021416350a
Woody97442 <woody97442@hotmail.fr>
2024-08-04 17:19:32 +0400

    Mise a jour de la bar de navigation

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: false,
+  // reactStrictMode: false, // À activer si vous voulez un mode strict pour React
   images: {
-    domains: ["9furg9ixi3wjuc9w.public.blob.vercel-storage.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "9furg9ixi3wjuc9w.public.blob.vercel-storage.com",
+        port: "", // Facultatif, si vous avez besoin d'un port spécifique
+        pathname: "/**", // Correspond à tous les chemins sous ce domaine
+      },
+    ],
   },
 };
 
